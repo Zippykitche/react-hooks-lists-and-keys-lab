ca95b08e30f5defb7a327f03ba6a51320acfa2b1
zippykitche <zippyk80@gmail.com>
2024-11-04 01:34:38 +0300

    completes lists and keys lab

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -3,7 +3,16 @@ import React from "react";
 function NavBar() {
   const links = ["home", "about", "projects"];
 
-  return <nav>{/* display an <a> tag for each link here */}</nav>;
+const NavBarLinks = links.map((link) => {
+  return <a key = {link} href = {`#${link}`} >
+     {link}
+  </a>  
+});
+  return (
+    <nav>
+     {NavBarLinks}
+    </nav>
+  );
 }
 
 export default NavBar;
